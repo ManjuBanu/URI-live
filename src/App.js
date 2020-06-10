09321d3745cb8c4931c2d05f535cf37878941b3d
v484407 <manjumala.devim@amerisourcebergen.com>
2020-06-10 19:46:38 +0530

    third-commit-10-6-20-routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
 import {Home} from './pages/home/home';
+import { Route } from 'react-router-dom';
+
+
+const Hats = (props) =>{
+  console.log('props',props);
+  return(
+  <div>
+  <h1>WE ARE IN HATS PAGE !!</h1>
+  </div>
+);
+};
+
 
 function App() {
   return (
     <div>
-    <Home/>
+    <Route exact path='/' component={Home}/>
+    <Route path='/hats' component={Hats}/>
     </div>
   );
 }
