a2daedf43a6f285ef22fcb47b54ce6b0791d203f
v484407 <manjumala.devim@amerisourcebergen.com>
2020-06-10 17:19:32 +0530

    second-commit-10-6-20-making-components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Home} from './home.jsx';
+import {Home} from './pages/home/home';
 
 function App() {
   return (
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './home.scss';
+import {Directory} from './../../components/directory/directory'
+
+export const Home =() =>(
+<div className="homepage">
+    <Directory/>
+</div>
+);
