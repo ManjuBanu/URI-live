9a7fc8067db0dcb15c324685e3440fd63d867cc0
v484407 <manjumala.devim@amerisourcebergen.com>
2020-06-10 16:30:23 +0530

    first-commit-10-6-20-homepage

---
+++ src/home.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './home.scss'
+
+export const Home =() =>(
+<div className="homepage">
+    <div className="directory-menu">
+        <div className="menu-item">
+            <div className="content">
+                <div className="title">HATS
+                    <div className="subtitle">Shop Now
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className="content">
+                <div className="title">HATS
+                    <div className="subtitle">Shop Now
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className="content">
+                <div className="title">HATS
+                    <div className="subtitle">Shop Now
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className="content">
+                <div className="title">HATS
+                    <div className="subtitle">Shop Now
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className="content">
+                <div className="title">HATS
+                    <div className="subtitle">Shop Now
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+);
