ca34f738f0f29e2cce5a960fdf3e7c02f39a79d8
v484407 <manjumala.devim@amerisourcebergen.com>
2020-06-24 18:01:22 +0530

    app.css removed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React,{useEffect} from 'react';
-import './App.css';
 import { Route,Switch, Redirect} from 'react-router-dom';
 import {Home} from './pages/home/home';
 import ShopPage from './pages/shop/shop-page';
